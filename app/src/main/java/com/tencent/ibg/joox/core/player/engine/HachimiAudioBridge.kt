48d4fb22b74a7c0235971023c2b2dd080131972e
7tattoo <7tattoo@users.noreply.github.com>
2026-09-25 06:49:16 +0800

    fix: add @file:OptIn(UnstableApi) to HachimiAudioBridge for lintDebug

--- app/src/main/java/com/tencent/ibg/joox/core/player/engine/HachimiAudioBridge.kt
+++ app/src/main/java/com/tencent/ibg/joox/core/player/engine/HachimiAudioBridge.kt
@@ -1,3 +1,5 @@
+@file:androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+
 package com.tencent.ibg.joox.core.player.engine
 
 import com.qing.hachimi.audio.EqualizerAudioProcessor
